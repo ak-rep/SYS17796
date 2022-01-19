513f3bd695909eb90225c801e103fdbd878ffcc7
ankit <kukretia@sheridancollege.ca>
2022-01-19 14:23:30 -0500

    First commit

---
+++ Week2/Person/src/person/Person.java
@@ -0,0 +1,19 @@
+
+package person;
+
+/**
+ * This class +++ insert class description here +++
+ *
+ * @author Ankit Kukreti
+ */
+public class Person {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+      
+    }
+
+}
---
+++ Week2/Person/src/person/PersonList.java
@@ -0,0 +1,11 @@
+
+package person;
+
+/**
+ * This class +++ insert class description here +++
+ *
+ * @author Ankit Kukreti
+ */
+public class PersonList {
+
+}
