6d9030463a1ff6d93b469b607a8b1c9c5dfa4275
ankit <kukretia@sheridancollege.ca>
2022-01-26 14:38:47 -0500

    Added HelloWorld2 java class in HelloWorld

---
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -0,0 +1,17 @@
+package helloworld;
+
+/**
+ *
+ * @author Ankit Kukreti
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        System.out.println("This is a hello world program");
+    }
+    
+}
---
+++ HelloWorld/src/helloworld/HelloWorld2.java
@@ -0,0 +1,16 @@
+
+package helloworld;
+
+/**
+ * This class +++ insert class description here +++
+ *
+ * @author Ankit Kukreti
+ */
+public class HelloWorld2 
+{
+    public void test()
+    {
+        System.err.println("This is a test member fucntion");
+    }
+
+}
